795c7d257f54bebb83ad62613f257728942f8e81
Wei Wang <wei.wang@extrinsec.com>
2023-01-25 19:57:38 -0500

    add ruby

---
+++ ruby/app.rb
@@ -0,0 +1,8 @@
+require 'appguard'
+require 'functions_framework'
+require 'net/http'
+
+FunctionsFramework.http 'demo_ruby' do |request|
+  Net::HTTP.get_response(URI('https://www.google.com'))
+  "Go Appguard!\n"
+end
