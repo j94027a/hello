e4ad8378099fdcda08b17f6c3afa3b3288c88994
Wei Wang <wei.wang@extrinsec.com>
2023-01-25 20:37:42 -0500

    add Gemfile.lock

--- ruby/app.rb
+++ ruby/app.rb
@@ -2,7 +2,7 @@ require 'appguard'
 require 'functions_framework'
 require 'net/http'
 
-FunctionsFramework.http 'demo_ruby' do |request|
+FunctionsFramework.http('demo_ruby') do |request|
   Net::HTTP.get_response(URI('https://www.google.com'))
   "Go Appguard!\n"
 end
